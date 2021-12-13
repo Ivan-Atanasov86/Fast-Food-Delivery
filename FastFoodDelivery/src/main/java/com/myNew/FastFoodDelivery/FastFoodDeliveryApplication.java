9391684a57ef9c1761e88e330c7538be52d057f2
Ivan-Atanasov86 <greenvarna1913@icloud.com>
2021-12-13 15:36:33 +0200

    Initalize commit

---
+++ FastFoodDelivery/src/main/java/com/myNew/FastFoodDelivery/FastFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.myNew.FastFoodDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ FastFoodDelivery/src/test/java/com/myNew/FastFoodDelivery/FastFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myNew.FastFoodDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
